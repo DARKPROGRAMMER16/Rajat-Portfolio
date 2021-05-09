cea5c02e9aac1855ffcdbd98d465264327dd6e77
Rajat Dash <apple@apples-MacBook-Air.local>
2021-05-09 13:30:51 +0530

    home and navbar css changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,24 +7,26 @@ const Home = () => {
 
     return (
         <Fragment>
-        <div className="container">
-            <div class="row h-100">
-                <div class="col-sm-12 my-auto">
-                    <div className="text-center">
-                        <h1 id="UName">Rajat Dash</h1>
-                        <h3 id="desc"><Typewriter
-                              options={{
-                                    strings: ['Web Developer', 'Tech Enthusiast','Full Stack Pratictioner'],
-                                    autoStart: true,
-                                    delay:'natural',
-                                    loop: true,
-                                }}
-                        /></h3>
-                        <div className="icons mt-3">
-                            <a href="/"><i className="fa fa-facebook" id="fb"></i></a>
-                            <a href="/"><i className="fa fa-instagram" id="ig"></i></a>
-                            <a href="/"><i className="fa fa-linkedin" id="ln"></i></a>
-                            <a href="/"><i className="fa fa-twitter" id="tw"></i></a>
+        <div className="home bg-dark">            
+            <div className="container">
+                <div className="row h-100">
+                    <div className="col-sm-12 my-auto">
+                        <div className="text-center">
+                            <h1 id="UName">Rajat Dash</h1>
+                            <h3 id="desc"><Typewriter
+                                options={{
+                                        strings: ['Web Developer', 'Tech Enthusiast','Full Stack Practitioner'],
+                                        autoStart: true,
+                                        delay:'natural',
+                                        loop: true,
+                                    }}
+                            /></h3>
+                            <div className="icons mt-3">
+                                <a href="/"><i className="fa fa-facebook" id="fb"></i></a>
+                                <a href="/"><i className="fa fa-instagram" id="ig"></i></a>
+                                <a href="/"><i className="fa fa-linkedin" id="ln"></i></a>
+                                <a href="/"><i className="fa fa-twitter" id="tw"></i></a>
+                            </div>
                         </div>
                     </div>
                 </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,8 +5,8 @@ import './Navbar.css'
 const Navbar = () => {
     return (
         <Fragment>            
-            <nav className="navbar navbar-expand-lg navbar-light bg-none">
-                <a className="navbar-brand ml-lg-5 navtitle" href="/">Rajat Dash</a>
+            <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+                <a className="navbar-brand ml-lg-5 navtitle" href="/"><h1>Rajat Dash</h1></a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -14,16 +14,16 @@ const Navbar = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ml-auto mr-lg-5 navtabs">
                         <li className="nav-item">
-                            <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+                            <a className="nav-link" href="/"><p>Home</p><span className="sr-only">(current)</span></a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/about">About</a>
+                            <a className="nav-link" href="/about"><p>About</p></a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/services">Services</a>
+                            <a className="nav-link" href="/services"><p>Services</p></a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/contact">Contact</a>
+                            <a className="nav-link" href="/contact"><p>Contact</p></a>
                         </li>
                         
                     </ul>
