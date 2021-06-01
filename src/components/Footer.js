e4cc724c75686e0ec534cda0e48002b121d237ed
Rajat Dash <rajatdash@Rajats-MacBook-Air.local>
2021-06-01 20:29:59 +0530

    footer component initialized

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+
+const Footer = () => {
+    return (
+        <Fragment>
+            <h1>hello footer</h1>
+        </Fragment>
+    )
+}
+
+export default Footer
